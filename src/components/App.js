2bd6a2108d8a7fb23e6b7e398f4dc7df83150144
Ben Marshall <benleem98@yahoo.com>
2022-03-07 23:41:55 -0500

    displaying all needed values on home page

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import Coins from "./coins/Coins"
 
 const App = () => {
     const [coins, setCoins] = useState([]);
-    const [coin, setCoin] = useState({})
-    const [coinsLoading, setCoinsLoading] = useState(false)
+    // const [coin, setCoin] = useState({})
+    const [coinsLoading, setCoinsLoading] = useState(false);
 
     const loadCoins = async () => {
         try {
             setCoinsLoading(true);
-            const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h%2C7d`);
+            const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h%2C7d`);
             setCoins(res.data);
             setCoinsLoading(false);
         } catch (err) {
@@ -24,16 +24,16 @@ const App = () => {
     }, [])
     
     useEffect(() => {
-        console.log(coins)
+        console.log(coins);
     }, [coins])
     
     return (
         <div className="App">
-            <Coins coins={coins} 
+            <Coins coins={coins}
             coinsLoading={coinsLoading}
             />
         </div>
     )
 }
 
-export default App
+export default App;
--- src/components/coins/Coin.js
+++ src/components/coins/Coin.js
@@ -1,11 +1,22 @@
 import styles from './coin.module.css';
 
 const Coin = ({ coin }) => {
-  return (
-    <div className={styles.coin}>
-        <p>{coin.name}</p>
-    </div>
-  )
+    return (
+        <div className={styles.coin}>
+            <img src={coin.image} alt="coin image"/>
+            <p className={styles.coinName}>{coin.name}</p>
+            <p className={styles.upperCase}>{coin.symbol}</p>
+            <p>{coin.current_price}</p>
+            <p className={coin.price_change_percentage_24h_in_currency > 0? styles.textGreen : styles.textRed}>
+                {Math.round(coin.price_change_percentage_24h_in_currency * 100) / 100}
+            </p>
+            <p className={coin.price_change_percentage_7d_in_currency > 0? styles.textGreen : styles.textRed}>
+                {Math.round(coin.price_change_percentage_7d_in_currency * 100) / 100}
+            </p>
+            <p>${coin.total_volume.toLocaleString("en-US")}</p>
+            <p>${coin.market_cap.toLocaleString("en-US")}</p>
+        </div>
+    )
 }
 
 export default Coin
